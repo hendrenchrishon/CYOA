5956af8bb2ecb5f72a3bb6cf152537c9f61c7fb5
hendrenchrishon <53783075+hendrenchrishon@users.noreply.github.com>
2019-12-19 09:26:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let doLogic = function(){
+
+let radioAge1= selection
+
+if( radioAge1== Snake){
+
+  alert("You Proceed because the snake was easy to step on.")
+
+}
+
+if( radioAge1== Bumble bee){
+
+  alert("You die because the medicine kits pills were too high of a dosage.")
+
+}
+
+if( radioAge1== dinosaur){
+
+  alerift("You survived because you were small enough to get in a corner and the arms were too small to get you.")
+
+} 
+if( radioAge1== bird){
+
+  alert("The birds peak reached you in the corner and pecked you to death.")
+
+}
+
+if( radioAge1== lizard){
+
+  alert("You Proceed because you are gentle and the lizard caused no harm.")
+
+}
+if( radioAge1== electric eel){
+
+  alert("You are dead because you were electricutied in the water.")
+
+}
+};
